1880bdd085797ff94b37ac6f647b5cb8923ddbde
shanto2575 <shanto2575@gmail.com>
2026-05-09 18:31:13 +0600

    details page

--- src/app/user/[id]/page.jsx
+++ src/app/user/[id]/page.jsx
@@ -1,12 +1,21 @@
 import { getUserById } from '@/app/lib/api/data';
+import { ArrowRight } from '@gravity-ui/icons';
+import { Button } from '@heroui/react';
+import Link from 'next/link';
 import React from 'react'
 
-const UserDetailsPage = async({params}) => {
-    const {id}=await params;
-    const user=await getUserById(id)
+const UserDetailsPage = async ({ params }) => {
+    const { id } = await params;
+    const user = await getUserById(id)
     console.log(user)
     return (
-        <div>UserDetails:{user.name} </div>
+        <div className='flex flex-col bg-gray-300 text-black font-bold text-2xl items-center justify-center  h-screen space-y-5'>
+            <h2 className='text-red-600'>User Details</h2>
+            <h2>Name : {user.name}</h2>
+            <p>Email : {user.email}</p>
+            <p>Role : {user.role}</p>
+            <Link href={'/user'}><Button>Go Back <ArrowRight/></Button></Link>
+        </div>
     )
 }
 
